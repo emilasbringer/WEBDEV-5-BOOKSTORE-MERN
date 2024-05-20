78f99d1b7576684d62a35f94d4b19fecb2e7aa35
emilasbringer <60388760+emilasbringer@users.noreply.github.com>
2024-05-20 15:50:41 +0200

    git

--- backend/index.js
+++ backend/index.js
@@ -10,20 +10,12 @@ const app = express();
 app.use(express.json());
 
 // Middleware for handling CORS POLICY
-// Option 1: Allow All Origins with Default of cors(*)
 app.use(cors());
 // Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
 
 app.get('/', (request, response) => {
   console.log(request);
-  return response.status(234).send('Welcome To MERN Stack Tutorial');
+  return response.status(234).send('Welcome To MERN Stack');
 });
 
 app.use('/books', booksRoute);
